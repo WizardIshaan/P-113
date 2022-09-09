d6326621c9717fd803b06beef7037b9c5193a2f1
WizardIshaan <95003425+WizardIshaan@users.noreply.github.com>
2022-09-08 19:08:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+function preload(){
+
+}
+
+function setup()
+{
+ canvas = createCanvas(640,480);
+ canvas.position(400, 250);
+ video = createCapture(VIDEO);
+ video.hide();
+}
+
+function draw()
+{
+  image(video, 120, 80, 400, 300);
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+  rect(50, 50, 20, 360);
+  rect(580, 50, 20, 360);
+  rect(50, 410, 550, 20);
+  rect(50, 30, 550, 20);
+}
+
+//function preload(){
+
+//}
+
+//function setup()
+//{
+  //canvas = createCanvas(640, 480);
+  //canvas.position(110, 250);
+  //video = createCapture(VIDEO);
+  //video.hide();
+  //tint_color = "";
+//}
+
+//function draw()
+//{
+  //image(video, 0, 0, 640, 480);
+  //tint(tint_color);
+//}
+
+//function take_snapshot()
+//{
+  //save('Ishaan.png');
+//}
+
+//function filter_tint()
+//{
+  //tint_color = document.getElementById("color_name").value;
+//}
+
+
+
+
+
+
+//background(256);
+//line( 78, 400, 1, 10)
+
+
+
+
